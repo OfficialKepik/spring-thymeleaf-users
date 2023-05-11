b1ef3ac5b5151cd772a1342a139d4643c2c2d7c8
gk <officialkepik@gmail.com>
2023-05-11 21:27:46 +0300

    j1105

---
+++ src/main/java/org/itstep/role/Role.java
@@ -0,0 +1,21 @@
+package org.itstep.role;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+}
---
+++ src/main/java/org/itstep/user/User.java
@@ -0,0 +1,36 @@
+package org.itstep.user;
+
+import lombok.Data;
+import org.itstep.role.Role;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    // remaining getters and setters are not shown for brevity
+}
